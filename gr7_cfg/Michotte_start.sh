8f8a40b905bf7a3549bf48f956480bfd1f085ad5
Antoine <antoine.rime@student.uclouvain.be>
2018-10-07 18:28:46 +0200

    Dummy routing policy

--- gr7_cfg/Michotte_start.sh
+++ gr7_cfg/Michotte_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+ip addr add fd00:200:7:3::/48 dev Michotte-eth0
+ip addr add fd00:200:7:3::/48 dev Michotte-eth1
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
--- gr7_cfg/Stevin_start.sh
+++ gr7_cfg/Stevin_start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+ip addr add fd00:200:7:5::/48 dev Stevin-eth0
+ip addr add fd00:200:7:5::/48 dev Stevin-eth1
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
