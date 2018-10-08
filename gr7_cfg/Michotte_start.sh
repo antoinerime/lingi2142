3c04ff7646f91bc257eb9a5a15da36f4ce53a202
Antoine <antoine.rime@student.uclouvain.be>
2018-10-08 18:06:01 +0200

    Add same sub on each interface

--- gr7_cfg/Michotte_start.sh
+++ gr7_cfg/Michotte_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip addr add fd00:200:7:30::/64 dev Michotte-eth0
-ip addr add fd00:200:7:31::/64 dev Michotte-eth1
+ip addr add fd00:200:7:43::0/64 dev Michotte-eth0
+ip addr add fd00:200:7:45::1/64 dev Michotte-eth1
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
--- gr7_cfg/Pythagore_start.sh
+++ gr7_cfg/Pythagore_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ip -6 addr add fd00:300::7/48 dev belneta
 
-ip addr add fd00:200:7:10::/64 dev Pythagore-eth0
-ip addr add fd00:200:7:11::/64 dev Pythagore-eth1
-ip addr add fd00:200:7:12::/64 dev Pythagore-eth2
+ip addr add fd00:200:7:21::0/64 dev Pythagore-eth0
+ip addr add fd00:200:7:25::1/64 dev Pythagore-eth1
+ip addr add fd00:200:7:26::2/64 dev Pythagore-eth2
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
