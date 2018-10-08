70d8e349b163246d275ca4336f9bbd0246c45365
Antoine <antoine.rime@student.uclouvain.be>
2018-10-08 16:48:13 +0200

    modify addr plan

--- gr7_cfg/Stevin_start.sh
+++ gr7_cfg/Stevin_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ip addr add fd00:200:7:5::1/64 dev Stevin-eth0
-ip addr add fd00:200:7:5::1/64 dev Stevin-eth1
+ip addr add fd00:200:7:50::/64 dev Stevin-eth0
+ip addr add fd00:200:7:51::/64 dev Stevin-eth1
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
