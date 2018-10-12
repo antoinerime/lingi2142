9504bfef9fcde7ce1409c31aaf9e82d73e9fab2d
Antoine <antoine.rime@student.uclouvain.be>
2018-10-12 10:37:23 +0200

    fix addr

--- gr7_cfg/Pythagore_start.sh
+++ gr7_cfg/Pythagore_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ip -6 addr add fd00:300::7/48 dev belneta
 
-ip addr add fd00:200:7:21::0/64 dev Pythagore-eth0
+ip addr add fd00:200:7:12::0/64 dev Pythagore-eth0
 ip addr add fd00:200:7:25::1/64 dev Pythagore-eth1
 ip addr add fd00:200:7:26::2/64 dev Pythagore-eth2
 
