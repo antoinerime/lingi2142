8a2cb8be6838879e336d75a55177e2da3b737537
decarvalhobo <marcio.decarvalho@student.uclouvain.be>
2018-10-15 15:02:46 +0200

    firewall - intern definition

---
+++ firewalls/ADDRESS.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# DNS
+DNS2=""
+DNS3=""
+
+# Router subnet address
+ROUT2="fd00:200:7:00::/56"
+ROUT3="fd00:300:7:00::/56"
+
+# Admin subnet address
+ADMIN2="fd00:200:7:10::/56"
+ADMIN3="fd00:300:7:10::/56"
+
+# Staff subnet address
+STAFF2="fd00:200:7:20::/56"
+STAFF3="fd00:300:7:20::/56"
+
+# Guest subnet address
+GUEST2="fd00:200:7:30::/56"
+GUEST3="fd00:300:7:30::/56"
+
+# IOT subnet address
+IOT2="fd00:200:7:40::/56"
+IOT3="fd00:300:7:40::/56"
