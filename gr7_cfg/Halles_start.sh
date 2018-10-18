f9189db605660599af82785db4bd41d06020cc4f
Antoine <antoine.rime@student.uclouvain.be>
2018-10-18 17:12:11 +0200

    Add route after bgp is established

--- gr7_cfg/Halles_start.sh
+++ gr7_cfg/Halles_start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-#ip route add fd00:200:7::/48 dev belnetb src fd00:200::7 table T200
-ip -6 route add default via fd00:200::b table 200
-ip -6 route add default via fd00:300:7:12::2 table 300
 
 
 
@@ -15,3 +12,8 @@ ip addr add fd00:200:7:13::0/64 dev Halles-eth0
 ip addr add fd00:200:7:12::1/64 dev Halles-eth1
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
+
+#Wait for ospf to converge
+sleep 30
+ip -6 route add default via fd00:200::b table 200
+ip -6 route add default via fd00:300:7:12::2 table 300
--- gr7_cfg/Pythagore_start.sh
+++ gr7_cfg/Pythagore_start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ip route add default via fd00:200:7:12::1 table 200
-ip route add default via fd00:300::b table 300
 
 
 ip -6 rule add from fd00:200:7::/48 table 200
@@ -14,3 +12,8 @@ ip addr add fd00:200:7:25::1/64 dev Pythagore-eth1
 ip addr add fd00:200:7:26::2/64 dev Pythagore-eth2
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
+
+#Wait for ospf to converge
+sleep 30
+ip route add default via fd00:200:7:12::1 table 200
+ip route add default via fd00:300::b table 300
