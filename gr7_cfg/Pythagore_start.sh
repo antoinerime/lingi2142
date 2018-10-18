b34a7bfb7f42e34062d71ff61198d2fc6c802ea8
adwid <adrien.widart@student.uclouvain.be>
2018-10-18 19:10:18 +0200

    Pythagore_start : ip addr add

--- gr7_cfg/Pythagore_start.sh
+++ gr7_cfg/Pythagore_start.sh
@@ -7,10 +7,12 @@ ip -6 addr add fd00:300::7/48 dev belneta
 ip addr add fd00:200:7:12::2/64 dev Pythagore-eth0
 ip addr add fd00:200:7:25::2/64 dev Pythagore-eth1
 ip addr add fd00:200:7:26::2/64 dev Pythagore-eth2
+ip addr add fd00:200:7:27::2/64 dev lan0-ADNS01
 
 ip addr add fd00:300:7:12::2/64 dev Pythagore-eth0
 ip addr add fd00:300:7:25::2/64 dev Pythagore-eth1
 ip addr add fd00:300:7:26::2/64 dev Pythagore-eth2
+ip addr add fd00:300:7:27::2/64 dev lan0-ADNS01
 
 puppet apply --verbose --parser future --hiera_config=/etc/puppet/hiera.yaml /etc/puppet/site.pp --modulepath=/puppetmodules
 
