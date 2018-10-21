d9c989c8db78a4c4960bcb469715c69a1fa92184
Anicet <asieweko@ulb.ac.be>
2018-10-21 08:44:43 +0000

    address allocation at the MONIT node

--- gr7_cfg/MONIT_start.sh
+++ gr7_cfg/MONIT_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ip addr add fd00:200:7:27::7/64 dev ADNS01-eth0
-ip addr add fd00:300:7:29::7/64 dev ADNS01-eth0
+ip addr add fd00:200:7:57::10/64 dev MONIT-eth0
+ip addr add fd00:300:7:57::10/64 dev MONIT-eth0
 
-ip -6 route add default via fd00:200:7:27::2
+ip -6 route add default via fd00:200:7:57::5
 
 /usr/sbin/named
