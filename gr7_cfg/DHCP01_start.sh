41322d6c2f6550e8fe01a870494b143a05567243
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-29 11:31:14 +0000

    DHCP : set dhcp config

--- gr7_cfg/DHCP01_start.sh
+++ gr7_cfg/DHCP01_start.sh
@@ -5,3 +5,4 @@ ip addr add fd00:300:7:2a::a/64 dev DHCP01-eth0
 
 ip -6 route add default via fd00:200:7:2a::2
 
+/usr/sbin/dhcpd -6 -cf /etc/dhcp/dhcpd6.conf -lf /etc/dhcp/dhcpd6.leases DHCP01-eth0
--- gr7_cfg/DHCP02_start.sh
+++ gr7_cfg/DHCP02_start.sh
@@ -5,3 +5,4 @@ ip addr add fd00:300:7:5a::a/64 dev DHCP02-eth0
 
 ip -6 route add default via fd00:200:7:5a::5
 
+/usr/sbin/dhcpd -6 -cf /etc/dhcp/dhcpd6.conf -lf /etc/dhcp/dhcpd6.leases DHCP02-eth0
