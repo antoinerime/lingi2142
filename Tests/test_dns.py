78d299ca958834e66ca4c8e7c832510cefd5c242
Vagrant Default User <vagrant@jessie.vagrantup.com>
2018-10-29 13:01:12 +0000

    test dns resolver on each router

--- Tests/test_dns.py
+++ Tests/test_dns.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
+import utils
 
-nodes = ["Halles", "Pythagore", "SH1C", "Michootte", "Stevin", "Carnoy"]
+nodes = ["Halles", "Pythagore", "SH1C", "Michotte", "Stevin", "Carnoy"]
 
 
 def test_dns():
     for node in nodes:
+   	print("Start pinging google.com on {}".format(node))
         out, err, err_code = utils.execute_in_host(node, "ping6 -c 1 -n -W2 google.com")
-    if err_code:
-        print("Could not ping google.com")
-        print(out)
-        print(err)
+        if err_code:
+            print("Could not ping google.com")
+            print(out)
+            print(err)
+
+if __name__ == '__main__':
+    test_dns()
