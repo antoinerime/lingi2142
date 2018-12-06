dee1e38956034d1ce8a331b0d98b417a4b96244a
decarvalhobo <marcio.decarvalho@student.uclouvain.be>
2018-12-06 22:13:50 +0100

    fix(firewalls) : fix typos of ports nbr fix protocol for dns fix response for snmp add spoof rules more fix comming...

--- firewalls/MICH.sh
+++ firewalls/MICH.sh
@@ -6,6 +6,7 @@
 #
 # -------- Initial configuration
 #
+
 # Erase all existing chain
 ip6tables -F INPUT
 ip6tables -F OUTPUT
@@ -35,10 +36,10 @@ ip6tables -A FORWARD -s fd00:200:7:11::1 -d fd00:200:7:22::2 -j ACCEPT
 ip6tables -A FORWARD -s fd00:200:7:22::2 -d fd00:200:7:11::1 -j ACCEPT
 
 # Accept DHCP
-ip6tables -A INPUT -p udp -m multiport --dport 547,547 -j ACCEPT
+ip6tables -A INPUT -p udp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A FORWARD -p udp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A OUTPUT -p udp -m multiport --dports 546,547 -j ACCEPT
-ip6tables -A INPUT -p tcp -m multiport --dport 547,547 -j ACCEPT
+ip6tables -A INPUT -p tcp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A FORWARD -p tcp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A OUTPUT -p tcp -m multiport --dports 546,547 -j ACCEPT
 
@@ -52,14 +53,12 @@ ip6tables -A INPUT -p 89 -j ACCEPT
 ip6tables -A OUTPUT -p 89 -j ACCEPT
 ip6tables -A FORWARD -p 89 -j ACCEPT
 
-# Accept Forwarding DNS queries/answers
-ip6tables -A FORWARD -d $SUB2 -p tcp --dport 53 -j ACCEPT
-ip6tables -A FORWARD -d $SUB3 -p tcp --dport 53 -j ACCEPT
-
 #
 # -------- ADMIN configuration
 #
-# Accept everything for/from administrators
+# Accept everything for/from administrators :
+# we don't accept to forward to destination admin to avoid user->admin
+# but accept forwarding from source admin
 ip6tables -A INPUT -s $ADMIN2 -j ACCEPT
 ip6tables -A INPUT -s $ADMIN3 -j ACCEPT
 ip6tables -A OUTPUT -d $ADMIN2 -j ACCEPT
@@ -70,36 +69,74 @@ ip6tables -A FORWARD -s $ADMIN3 -j ACCEPT
 #
 # -------- STAFF configuration
 #
-# Drop forwarding ssh/dns/http/https packet from staff to staff
-ip6tables -A FORWARD -s $STAFF2  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF2  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Drop forwarding ssh/dns/http/https packet from staff to guest
-ip6tables -A FORWARD -s $STAFF2  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF2  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Accept forwarding ssh/dns/http/https packet from staff
-ip6tables -A FORWARD -s $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j ACCEPT
-ip6tables -A FORWARD -s $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j ACCEPT
+
+# STAFF --> STAFF
+# Drop forwarding ssh/http/https packet from staff to staff (on tcp)
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from staff to staff (on udp)
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF3 -p udp --dport 53 -j DROP
+
+# STAFF --> GUEST
+# Drop forwarding ssh/http/https packet from staff to guest (on tcp)
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from staff to guest (on udp)
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST3 -p udp --dport 53 -j DROP
+
+# STAFF --> SERVICES
+# Accept tcp : forwarding http/https
+ip6tables -A FORWARD --src $STAFF2 -p tcp -m multiport --dports 80,443 -j ACCEPT
+ip6tables -A FORWARD --src $STAFF3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+# Accept udp : forwarding dns
+ip6tables -A FORWARD --src $STAFF2 -p udp --dport 53 -j ACCEPT
+ip6tables -A FORWARD --src $STAFF3 -p udp --dport 53 -j ACCEPT
 
 #
 # -------- GUEST configuration
 #
-# Drop forwarding ssh/dns/http/https packet from guest to staff
-ip6tables -A FORWARD -s $GUEST2  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST2  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Drop forwarding ssh/dns/http/https packet from guest to guest
-ip6tables -A FORWARD -s $GUEST2  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST2  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Accept tcp : send dns , http/https
-ip6tables -A FORWARD --src $GUEST2 -p tcp -m multiport --dports 53,80,443 -j ACCEPT
-ip6tables -A FORWARD --src $GUEST3 -p tcp -m multiport --dports 53,80,443 -j ACCEPT
+
+# GUEST --> STAFF
+# Drop forwarding ssh/http/https packet from guest to staff (on tcp)
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from guest to staff (on udp)
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF3 -p udp --dport 53 -j DROP
+
+# GUEST --> GUEST
+# Drop forwarding ssh/http/https packet from guest to guest (on tcp)
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from guest to guest (on udp)
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST3 -p udp --dport 53 -j DROP
+
+# GUEST --> SERVICES
+# Accept tcp : forwarding http/https
+ip6tables -A FORWARD --src $GUEST2 -p tcp -m multiport --dports 80,443 -j ACCEPT
+ip6tables -A FORWARD --src $GUEST3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+# Accept udp : forwarding dns
+ip6tables -A FORWARD --src $GUEST2 -p udp --dport 53 -j ACCEPT
+ip6tables -A FORWARD --src $GUEST3 -p udp --dport 53 -j ACCEPT
 
 #
 # -------- IOT configuration
@@ -108,10 +145,14 @@ ip6tables -A FORWARD --src $GUEST3 -p tcp -m multiport --dports 53,80,443 -j ACC
 ip6tables -A FORWARD --src $STAFF2 -p tcp --dport 515 -j ACCEPT
 ip6tables -A FORWARD --src $STAFF3 -p tcp --dport 515 -j ACCEPT
 
-# Accept SNMP  protocol
-ip6tables -A INPUT -p udp --dport 161 -j ACCEPT
-ip6tables -A OUTPUT -p udp --dport 161 -j ACCEPT
-ip6tables -A FORWARD -p udp --dport 161 -j ACCEPT
+# Accept Forwarding DNS queries/answers (at the bottom to avoid catch with user rules)
+ip6tables -A FORWARD -d $SUB2 -p udp --dport 53 -j ACCEPT
+ip6tables -A FORWARD -d $SUB3 -p udp --dport 53 -j ACCEPT
+
+# Accept SNMP  protocol (request and response on udp)
+ip6tables -A INPUT -p udp -m multiport --dports 161,162 -j ACCEPT
+ip6tables -A OUTPUT -p udp -m multiport --dports 161,162 -j ACCEPT
+ip6tables -A FORWARD -p udp -m multiport --dports 161,162 -j ACCEPT
 
 # Log
 ip6tables -A INPUT -j LOG
--- firewalls/PYTH.sh
+++ firewalls/PYTH.sh
@@ -6,6 +6,7 @@
 #
 # -------- Initial configuration
 #
+
 # Erase all existing chain
 ip6tables -F INPUT
 ip6tables -F OUTPUT
@@ -26,16 +27,20 @@ ip6tables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 ip6tables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 ip6tables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
+#
+# -------- Tunnel configuration
+#
+
 #Accept Tunnel protocol
 ip6tables -A FORWARD -s fd00:200:7:11::1 -d fd00:200:7:22::2 -j ACCEPT
 ip6tables -A FORWARD -s fd00:200:7:22::2 -d fd00:200:7:11::1 -j ACCEPT
 ip6tables -A INPUT -s fd00:200:7:11::1 -d fd00:200:7:22::2 -j ACCEPT
 ip6tables -A OUTPUT -d fd00:200:7:11::1 -j ACCEPT
 
-
 #
 # -------- Border configuration
 #
+
 # Block spoofed address from internet interface
 ip6tables -A INPUT -i belneta -s $SUB2 -j DROP
 ip6tables -A FORWARD -i belneta -s $SUB2 -j DROP
@@ -52,11 +57,8 @@ ip6tables -A OUTPUT -o belneta -p 89 -j DROP
 ip6tables -A FORWARD -i belneta -p 89 -j DROP
 ip6tables -A FORWARD -o belneta -p 89 -j DROP
 
-# Block SSH (port:22 over tcp) connection from outside
-ip6tables -A INPUT -i belneta -p tcp --dport 22 -j DROP
-
 # Block HTTP/HTTPS (port:80,443 over tcp) request from outside
-ip6tables -A INPUT -i belneta -p tcp -m multiport --dport 80,443 -j DROP
+ip6tables -A INPUT -i belneta -p tcp -m multiport --dports 80,443 -j DROP
 
 # Block DHCPv6 (port:546,547 over udp) from/to Internet
 ip6tables -A INPUT -i belneta -p udp -m multiport --dports 546,547 -j DROP
@@ -64,44 +66,40 @@ ip6tables -A OUTPUT -o belneta -p udp -m multiport --dports 546,547 -j DROP
 ip6tables -A FORWARD -i belneta -p udp -m multiport --dports 546,547 -j DROP
 ip6tables -A FORWARD -o belneta -p udp -m multiport --dports 546,547 -j DROP
 
-# Block SSH (port:22 over tcp) connection from outside
-ip6tables -A INPUT -i belnetb -p tcp --dport 22 -j DROP
-
 #
 # -------- ROUTER configuration + DNS Server
 #
 
 # Accept DHCP
-ip6tables -A INPUT -p udp -m multiport --dport 547,547 -j ACCEPT
+ip6tables -A INPUT -p udp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A FORWARD -p udp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A OUTPUT -p udp -m multiport --dports 546,547 -j ACCEPT
-ip6tables -A INPUT -p tcp -m multiport --dport 547,547 -j ACCEPT
+ip6tables -A INPUT -p tcp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A FORWARD -p tcp -m multiport --dports 546,547 -j ACCEPT
 ip6tables -A OUTPUT -p tcp -m multiport --dports 546,547 -j ACCEPT
-
-# Accept ospf (port:89) from inside the Network
-ip6tables -A INPUT -p 89 -j ACCEPT
-ip6tables -A OUTPUT -p 89 -j ACCEPT
-ip6tables -A FORWARD -p 89 -j ACCEPT
 
 # Accept ICMPv6
 ip6tables -A INPUT -p icmpv6 -j ACCEPT
 ip6tables -A OUTPUT -p icmpv6 -j ACCEPT
 ip6tables -A FORWARD -p icmpv6 -j ACCEPT
 
+# Accept ospf (port:89) from inside the Network
+ip6tables -A INPUT -p 89 -j ACCEPT
+ip6tables -A OUTPUT -p 89 -j ACCEPT
+ip6tables -A FORWARD -p 89 -j ACCEPT
+
 # Accept incomming and outgoing packet for DNS
-ip6tables -A INPUT -s $SUB2 -p tcp --dport 53 -j ACCEPT
-ip6tables -A OUTPUT -d $SUB2-p tcp --dport 53 -j ACCEPT
-ip6tables -A FORWARD -d $SUB2 -p tcp --dport 53 -j ACCEPT
-ip6tables -A INPUT -s $SUB3 -p tcp --dport 53 -j ACCEPT
-ip6tables -A OUTPUT -d $SUB3 -p tcp --dport 53 -j ACCEPT
-ip6tables -A FORWARD -d $SUB3 -p tcp --dport 53 -j ACCEPT
+ip6tables -A INPUT -s $SUB2 -p udp --dport 53 -j ACCEPT
+ip6tables -A OUTPUT -d $SUB2-p udp --dport 53 -j ACCEPT
+ip6tables -A INPUT -s $SUB3 -p udp --dport 53 -j ACCEPT
+ip6tables -A OUTPUT -d $SUB3 -p udp --dport 53 -j ACCEPT
 
 #
 # -------- ADMIN configuration
 #
-
-# Accept everything for/from administrators
+# Accept everything for/from administrators :
+# we don't accept to forward to destination admin to avoid user->admin
+# but accept forwarding from source admin
 ip6tables -A INPUT -s $ADMIN2 -j ACCEPT
 ip6tables -A INPUT -s $ADMIN3 -j ACCEPT
 ip6tables -A OUTPUT -d $ADMIN2 -j ACCEPT
@@ -112,36 +110,74 @@ ip6tables -A FORWARD -s $ADMIN3 -j ACCEPT
 #
 # -------- STAFF configuration
 #
-# Drop forwarding ssh/dns/http/https packet from staff to staff
-ip6tables -A FORWARD -s $STAFF2  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF2  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Drop forwarding ssh/dns/http/https packet from staff to guest
-ip6tables -A FORWARD -s $STAFF2  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF2  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $STAFF3  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Accept forwarding ssh/dns/http/https packet from staff
-ip6tables -A FORWARD -s $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j ACCEPT
-ip6tables -A FORWARD -s $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j ACCEPT
+
+# STAFF --> STAFF
+# Drop forwarding ssh/http/https packet from staff to staff (on tcp)
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from staff to staff (on udp)
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $STAFF3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $STAFF3 -p udp --dport 53 -j DROP
+
+# STAFF --> GUEST
+# Drop forwarding ssh/http/https packet from staff to guest (on tcp)
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from staff to guest (on udp)
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF2  -d $GUEST3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $STAFF3  -d $GUEST3 -p udp --dport 53 -j DROP
+
+# STAFF --> SERVICES
+# Accept tcp : forwarding http/https
+ip6tables -A FORWARD --src $STAFF2 -p tcp -m multiport --dports 80,443 -j ACCEPT
+ip6tables -A FORWARD --src $STAFF3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+# Accept udp : forwarding dns
+ip6tables -A FORWARD --src $STAFF2 -p udp --dport 53 -j ACCEPT
+ip6tables -A FORWARD --src $STAFF3 -p udp --dport 53 -j ACCEPT
 
 #
 # -------- GUEST configuration
 #
-# Drop forwarding ssh/dns/http/https packet from guest to staff
-ip6tables -A FORWARD -s $GUEST2  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST2  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $STAFF2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $STAFF3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Drop forwarding ssh/dns/http/https packet from guest to guest
-ip6tables -A FORWARD -s $GUEST2  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST2  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $GUEST2 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-ip6tables -A FORWARD -s $GUEST3  -d $GUEST3 -p tcp -m multiport --dports 22,53,80,443 -j DROP
-# Accept tcp : send dns , http/https
-ip6tables -A FORWARD --src $GUEST2 -p tcp -m multiport --dports 53,80,443 -j ACCEPT
-ip6tables -A FORWARD --src $GUEST3 -p tcp -m multiport --dports 53,80,443 -j ACCEPT
+
+# GUEST --> STAFF
+# Drop forwarding ssh/http/https packet from guest to staff (on tcp)
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from guest to staff (on udp)
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $STAFF3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $STAFF3 -p udp --dport 53 -j DROP
+
+# GUEST --> GUEST
+# Drop forwarding ssh/http/https packet from guest to guest (on tcp)
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST2 -p tcp -m multiport --dports 22,80,443 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST3 -p tcp -m multiport --dports 22,80,443 -j DROP
+# Drop forwarding dns packet from guest to guest (on udp)
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST2  -d $GUEST3 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST2 -p udp --dport 53 -j DROP
+ip6tables -A FORWARD -s $GUEST3  -d $GUEST3 -p udp --dport 53 -j DROP
+
+# GUEST --> SERVICES
+# Accept tcp : forwarding http/https
+ip6tables -A FORWARD --src $GUEST2 -p tcp -m multiport --dports 80,443 -j ACCEPT
+ip6tables -A FORWARD --src $GUEST3 -p tcp -m multiport --dports 80,443 -j ACCEPT
+# Accept udp : forwarding dns
+ip6tables -A FORWARD --src $GUEST2 -p udp --dport 53 -j ACCEPT
+ip6tables -A FORWARD --src $GUEST3 -p udp --dport 53 -j ACCEPT
 
 #
 # -------- IOT configuration
@@ -150,15 +186,14 @@ ip6tables -A FORWARD --src $GUEST3 -p tcp -m multiport --dports 53,80,443 -j ACC
 ip6tables -A FORWARD --src $STAFF2 -p tcp --dport 515 -j ACCEPT
 ip6tables -A FORWARD --src $STAFF3 -p tcp --dport 515 -j ACCEPT
 
-# Accept SNMP  protocol (experimental)
-ip6tables -A INPUT -p udp --dport 161 -j ACCEPT
-ip6tables -A OUTPUT -p udp --dport 161 -j ACCEPT
-ip6tables -A FORWARD -p udp --dport 161 -j ACCEPT
+# Accept Forwarding DNS queries/answers (at the bottom to avoid catch with user rules)
+ip6tables -A FORWARD -d $SUB2 -p udp --dport 53 -j ACCEPT
+ip6tables -A FORWARD -d $SUB3 -p udp --dport 53 -j ACCEPT
 
-# Accept SSH connection (port:22) (to be filtered: A)
-ip6tables -A INPUT -p tcp --dport 22 -j ACCEPT
-ip6tables -A OUTPUT -p tcp --dport 22 -j ACCEPT
-ip6tables -A FORWARD -p tcp --dport 22 -j ACCEPT
+# Accept SNMP  protocol (request and response on udp)
+ip6tables -A INPUT -p udp -m multiport --dports 161,162 -j ACCEPT
+ip6tables -A OUTPUT -p udp -m multiport --dports 161,162 -j ACCEPT
+ip6tables -A FORWARD -p udp -m multiport --dports 161,162 -j ACCEPT
 
 # Log
 ip6tables -A INPUT -j LOG
